7268cd10b1cec2463d0f87bc727420a3affa01c9
Julian Knorr <git@jknorr.eu>
2020-12-08 21:53:54 +0100

    Implement Task 2

--- Day_08/day08.py
+++ Day_08/day08.py
@@ -31,13 +31,17 @@ def execute_operation(operation: str) -> Tuple[int, int]:
     raise ValueError("Invalid Operation.")
 
 
-def execute_code(program: List[str]) -> int:
+def execute_code(program: List[str]) -> Tuple[int, bool]:
     executed = [False] * len(program)
     acc = 0
     pointer = 0
     while True:
+        if pointer == len(program):
+            return acc, True
+        if pointer >= len(program):
+            return acc, False
         if executed[pointer]:
-            return acc
+            return acc, False
         acc_diff, pointer_diff = execute_operation(program[pointer])
         executed[pointer] = True
         acc += acc_diff
@@ -45,7 +49,23 @@ def execute_code(program: List[str]) -> int:
 
 
 def task1(puzzle: List[str]) -> int:
-    return execute_code(puzzle)
+    return (execute_code(puzzle))[0]
+
+
+def task2(puzzle: List[str]) -> int:
+    for i in range(-1, len(puzzle)):
+        modified_program = puzzle.copy()
+        if i >= 0:
+            if modified_program[i].startswith("nop "):
+                modified_program[i] = "jmp" + modified_program[i][3:]
+            elif modified_program[i].startswith("jmp "):
+                modified_program[i] = "nop" + modified_program[i][3:]
+            else:
+                continue
+        acc, success = execute_code(modified_program)
+        if success:
+            return acc
+    raise RuntimeError("Task 2 not solvable.")
 
 
 if __name__ == '__main__':
@@ -54,5 +74,7 @@ if __name__ == '__main__':
         puzzle_lines = read_puzzle_file(input_file)
         task1_solution = task1(puzzle_lines)
         print("Task 1: acc = {:d}".format(task1_solution))
+        task2_solution = task2(puzzle_lines)
+        print("Task 2: acc = {:d}".format(task2_solution))
     else:
         print("Usage: {:s} puzzle file".format(sys.argv[0]))
