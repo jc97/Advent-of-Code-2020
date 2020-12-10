461c1b930f6b1c0a81da508681576ed55353a7dd
Julian Knorr <git@jknorr.eu>
2020-12-10 22:29:37 +0100

    Implement Task 1

---
+++ Day_10/day10.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+"""Advent of Code 2020 - Day 10 - Solution by Julian Knorr (git@jknorr.eu)"""
+import sys
+from typing import List
+
+
+def read_puzzle_file(filename: str) -> List[int]:
+    file = open(filename, 'r')
+    puzzle = []
+    for line in file.readlines():
+        puzzle.append(int(line))
+    puzzle.sort()
+    return puzzle
+
+
+def puzzle_amend(puzzle: List[int]) -> None:
+    puzzle.append(max(*puzzle) + 3)
+
+
+def task1(puzzle: List[int]) -> int:
+    current_adapter = 0
+    joltage_differences_count = {1: 0, 2: 0, 3: 0}
+    joltage_differences_count[puzzle[0]] += 1
+    while current_adapter < len(puzzle) - 1:
+        joltage_difference = puzzle[current_adapter + 1] - puzzle[current_adapter]
+        if joltage_difference > 3:
+            raise RuntimeError("Not solvable.")
+        joltage_differences_count[joltage_difference] += 1
+        current_adapter += 1
+    return joltage_differences_count[1] * joltage_differences_count[3]
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        input_file = sys.argv[1]
+        puzzle_lines = read_puzzle_file(input_file)
+        puzzle_amend(puzzle_lines)
+        task1_solution = task1(puzzle_lines)
+        print("Task 1: {:d}".format(task1_solution))
+    else:
+        print("Usage: {:s} puzzle file".format(sys.argv[0]))
