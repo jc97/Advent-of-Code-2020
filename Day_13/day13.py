3471b473fdfe6d349e9c649fb2676b0cb864725d
Julian Knorr <git@jknorr.eu>
2020-12-13 18:21:23 +0100

    Implement Day 13 Task 2

--- Day_13/day13.py
+++ Day_13/day13.py
@@ -24,6 +24,36 @@ def task1(schedule: str, departure: int) -> int:
     return best_bus["id"] * best_bus["waiting"]
 
 
+def task2(schedule: str) -> int:
+    first_bus = None
+    for bus in schedule.strip().split(","):
+        if bus != "x":
+            first_bus = int(bus)
+            break
+    t = first_bus
+    busses = schedule.strip().split(",")
+    iteration_factor = first_bus
+    iteration_factor_index = 0
+    while True:
+        valid_result = True
+        i = -1
+        for bus in busses:
+            i += 1
+            if bus == "x" or int(bus) == first_bus:
+                continue
+            bus_int = int(bus)
+            check = (t + busses.index(bus)) % bus_int
+            if check != 0 and check != bus_int:
+                valid_result = False
+                break
+            elif iteration_factor_index < i:
+                iteration_factor *= bus_int
+                iteration_factor_index = i
+        if valid_result:
+            return t
+        t += iteration_factor
+
+
 def read_puzzle_file(filename: str) -> Tuple[str, int]:
     file = open(filename, 'r')
     puzzle = file.readlines()
@@ -38,5 +68,7 @@ if __name__ == '__main__':
         puzzle_schedule, puzzle_departure = read_puzzle_file(input_file)
         task1_solution = task1(puzzle_schedule, puzzle_departure)
         print("Task 1: {:d}".format(task1_solution))
+        task2_solution = task2(puzzle_schedule)
+        print("Task 2: {:d}".format(task2_solution))
     else:
         print("Usage: {:s} puzzle file".format(sys.argv[0]))
