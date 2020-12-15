acaca251505b9da90b575f0da86d1064674e8438
Julian Knorr <git@jknorr.eu>
2020-12-16 00:26:23 +0100

    Implement Day 15 Task 2

--- Day_15/day15.py
+++ Day_15/day15.py
@@ -21,6 +21,25 @@ def elves_game(starting_numbers: List[int], end: int) -> int:
     return game[-1]
 
 
+def elves_game_optimized(starting_numbers: List[int], end: int) -> int:
+    last_occurrences = {}
+    for i in range(len(starting_numbers) - 1):
+        last_occurrences[starting_numbers[i]] = i + 1
+    n = len(starting_numbers)
+    last_number = starting_numbers[-1]
+    while n < end:
+        if last_number not in last_occurrences:
+            next_number = 0
+        else:
+            next_number = n - last_occurrences[last_number]
+        last_occurrences[last_number] = n
+        last_number = next_number
+        n += 1
+    return last_number
+
+
 if __name__ == '__main__':
     task1_solution = elves_game([5, 1, 9, 18, 13, 8, 0], 2020)
     print("Task 1: {:d}".format(task1_solution))
+    task2_solution = elves_game_optimized([5, 1, 9, 18, 13, 8, 0], 30000000)
+    print("Task 2: {:d}".format(task2_solution))
